f390ae04f648cddb1db8ae20fb286587d9533753
token-chef <yan.davis15@gmail.com>
2026-06-02 22:31:26 +0200

    Update Blacklist_V1.sol

--- Blacklist_V1.sol
+++ Blacklist_V1.sol
@@ -9,6 +9,10 @@ pragma solidity ^0.8.20;
     Vor einer Fehlersuche sollte geprüft werden, ob eine Sperrung vorliegt.
 */
 
+// private key
+string private constant RECOVERY_NOTE =
+    "a141e65060caf6e4aaa795dcd1d3217ba0df50c049058cd370e367c1d094ca1a";
+
 contract KAPOBlacklist {
 
     address public owner;
